0037e1dca4c130506580dac0e931c13151a11bf7
Lazaro <lachy200408@gmail.com>
2024-12-03 09:51:52 -0500

    feat: Adding persistence to switch theme feature

--- web/scripts/switch-theme.js
+++ web/scripts/switch-theme.js
@@ -4,7 +4,20 @@ if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
   $('.theme-btn').setAttribute('data-theme', 'dark')
 }
 
+if (sessionStorage.getItem('theme') === 'dark') {
+  $('.theme-btn').classList.add('dark')
+  document.documentElement.classList.add('dark')
+} else if (sessionStorage.getItem('theme') === 'light') {
+  $('.theme-btn').classList.remove('dark')
+  document.documentElement.classList.remove('dark')
+}
+
 $('.theme-btn').addEventListener('click', () => {
+  const root = document.documentElement
   $('.theme-btn').classList.toggle('dark')
-  document.documentElement.classList.toggle('dark')
+  root.classList.toggle('dark')
+  sessionStorage.setItem(
+    'theme',
+    root.classList.contains('dark') ? 'dark' : 'light'
+  )
 })
