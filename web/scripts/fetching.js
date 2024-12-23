7baa85ed4dbd278b931ebdf67b9c5ef88a5c8905
Lazaro <lachy200408@gmail.com>
2024-12-22 20:17:31 -0500

    mod: Dispuesto para producción

--- web/scripts/fetching.js
+++ web/scripts/fetching.js
@@ -2,12 +2,12 @@ const RESOURCE_URL = 'https://atuksa-catalog.onrender.com'
 
 export const fetchCatalog = (section, callback) => {
   //* Development
-  fetch('/dev-catalog-site/games.json')
-    .then((res) => res.json())
-    .then(callback)
-
-  //* Production
-  // fetch(`${RESOURCE_URL}/${section}`)
+  // fetch('/dev-catalog-site/games.json')
   //   .then((res) => res.json())
   //   .then(callback)
+
+  //* Production
+  fetch(`${RESOURCE_URL}/${section}`)
+    .then((res) => res.json())
+    .then(callback)
 }
